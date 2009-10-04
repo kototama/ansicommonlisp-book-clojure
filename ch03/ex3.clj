814c417458a31eccf80b6ae6d728c10d78eb3ce7
kototama <kototamo+code@gmail.com>
2009-10-04 15:12:09 +0200

    ch03/ex3

---
+++ ch03/ex3.clj
@@ -0,0 +1,13 @@
+(use 'clojure.contrib.def)
+
+(declare count-occurences)
+
+(defn occurences [col]
+  (reverse (sort-by (fn [[v occ]] occ) (count-occurences col))))
+
+(defn- count-occurences [col]
+  "return a map key/nb_occurences"
+  (reduce (fn [m v] (assoc m v (inc (get m v 0)))) {} col))
+
+
+(occurences '(a b a d a c d c a)) ;; ([a 4] [d 2] [c 2] [b 1])
