f30576f182c794059b8a54b7a9db3ca41af1041c
kototama <kototamo+code@gmail.com>
2009-10-04 16:28:52 +0200

    ch03/ex8

---
+++ ch03/ex8.clj
@@ -0,0 +1,14 @@
+;; dot notation and conses are specific to Lisp but let us
+;; nonetheless implement the function
+
+(defn showdots [col]
+  (if-let [elt (first col)]
+    (do
+      (print "(")
+      (printf "%s . " elt)
+      (showdots (rest col))
+      (print ")"))
+    (print "nil")))
+
+
+(showdots '(a b c)) ;; output (a . (b . (c . nil)))
