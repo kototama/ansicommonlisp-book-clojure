b1e88efc0c4da66ffd438a9a39f92eddf85aaf65
kototama <kototamo+code@gmail.com>
2009-11-15 17:59:38 +0100

    implementation of rotate with lazy sequences

--- ch04/ex1.clj
+++ ch04/ex1.clj
@@ -2,11 +2,15 @@
 (defn rotate [m]
   (apply vector (apply map (fn [& args] (apply vector (reverse args))) m)))
 
+(defn rotate-seq [m]
+  "rotate vector m, returns a lazy sequence of lazy sequences"
+  (apply map (comp reverse vector) m))
 
 (comment
 
   (= (rotate [[:a :b] [:c :d]]) [[:c :a] [:d :b]])
-  
+  (= (into [] (rotate-seq [[:a :b] [:c :d]])) ['(:c :a) '(:d :b)])
+
   (= (rotate [[:a :b :c] [:d :e :f] [:g :h :i]]) [[:g :d :a] [:h :e :b] [:i :f :c]])
 )
 
