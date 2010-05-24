90285db759a379b1242f0f05294905173e54920a
kototama <kototamo+code@gmail.com>
2010-05-24 12:47:01 +0200

    replace '() by ()

--- src/acl/ch04/ex2.clj
+++ src/acl/ch04/ex2.clj
@@ -1,7 +1,7 @@
 (ns acl.ch04.ex2)
 ;; makes not so much sense since values are immutables
 (defn copy-list [l]
-  (reverse (reduce (fn [acc x] (cons x acc)) '() l)))
+  (reverse (reduce (fn [acc x] (cons x acc)) () l)))
 
 (defn my-reverse [l]
   (reduce (fn [acc x] (cons x acc)) () l))
